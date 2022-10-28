9d657e4afe3090f7a08064381941311a2d158e8b
Oblakulov Mukhammadamin <moblakulov5@gmail.com>
2022-10-28 19:34:39 +0500

    a

---
+++ src/test/java/com/cydeo/step_definitions/EatingCucumber.java
@@ -0,0 +1,22 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class EatingCucumber {
+
+    @Given("amin is hungary")
+    public void amin_is_hungary() {
+        System.out.println("it is running from Given");
+    }
+    @When("amin is eating cucumber")
+    public void amin_is_eating_cucumber() {
+        System.out.println("it is running from When");
+    }
+    @Then("he is full")
+    public void he_is_full() {
+        System.out.println("it is running from Then");
+    }
+
+}
---
+++ src/test/resources/features/eating_cucumber.feature
@@ -0,0 +1,6 @@
+Feature: Eating too many cucumber is not good for you
+  user story: eating not too many may not be problem
+  Scenario:
+    Given amin is hungary
+    When amin is eating cucumber
+    Then he is full
